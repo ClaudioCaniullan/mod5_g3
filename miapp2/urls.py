2646101dbdcf5f62ac784f72080c729ef82f3325
ClaudioCaniullan <30704079+ClaudioCaniullan@users.noreply.github.com>
2021-01-06 16:52:43 -0300

    se agrega comentarios
    se agregan comentarios para poder abordar las tareas pendientes

--- miapp2/urls.py
+++ miapp2/urls.py
@@ -5,8 +5,11 @@ from . import views
 
 
 urlpatterns = [
+    # path para ver html extendido y base
     path("extendido/", views.mostrar_extendido),
     path("base/", views.mostrar_base),
+    
+    # path de pruebas para ver formulario contacto y contador de palabras
     path("contacto/", views.contacto),
     path("palabras/", views.contador_palabras),
   
